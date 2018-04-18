28ac2ccf8b6d7f48632783f124537c76b71e27bb
Brent Kie <brent.a.kie@gmail.com>
2018-04-18 17:49:16 -0600

    Updated goal section.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -8,11 +8,15 @@
 		<h1>PWP Milestone 1</h1>
 			<h2>Purpose, Audience, Goal</h2>
 				<h3>Purpose</h3>
-					<p>This project will be a professional portfolio website that will focus on my progressions as a full-stack web developer</p>
+					<p>This project will be a professional portfolio website that will focus on my progressions as a fullstack web developer</p>
 				<h3>Audience</h3>
 					<p>This website is designed for a target audience of professionals in web development and IT. More specifically,
 					the audience includes web developers, project managers, recruiters, tech startup companies, and entrepreneurs.
 				<h3>Goal</h3>
+					<p>The main goal of this website is for the target audience to see my work as a fullstack web developer with
+					an underlying notion of creating future collaborations or possible employment. The use of a contact form and social links
+					on the website will provide the target audience multiple channels to connect with me. The secondary goal of this
+					website is to showcase my work and contributions to projects as well as creating a professional online presence. </p>
 			<h2>Persona</h2>
 	</body>
 </html>
