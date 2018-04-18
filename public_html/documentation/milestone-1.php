3bed4b6555fc7eba35ec10d2b29d8c57fc1b5611
Brent Kie <brent.a.kie@gmail.com>
2018-04-18 16:35:33 -0600

    Updated purpose section.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset = "utf-8">
+		<meta charset="utf-8">
 		<title>Demo Project</title>
 	</head>
 	<body>
 		<h1>PWP Milestone 1</h1>
 			<h2>Purpose, Audience, Goal</h2>
 				<h3>Purpose</h3>
+					<p>This project will be a professional portfolio website that will focus on my progressions as a full-stack web developer</p>
 				<h3>Audience</h3>
 				<h3>Goal</h3>
 			<h2>Persona</h2>
