535f90d351271772ead4933abaaf6c00a9d18b76
Brent Kie <brent.a.kie@gmail.com>
2018-04-18 16:43:27 -0600

    Updated audience section.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -10,6 +10,8 @@
 				<h3>Purpose</h3>
 					<p>This project will be a professional portfolio website that will focus on my progressions as a full-stack web developer</p>
 				<h3>Audience</h3>
+					<p>This website is designed for a target audience of professionals in web development and IT. More specifically,
+					the audience includes web developers, project managers, recruiters, tech startup companies, and entrepreneurs.
 				<h3>Goal</h3>
 			<h2>Persona</h2>
 	</body>
