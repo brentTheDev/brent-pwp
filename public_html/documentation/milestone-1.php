5f3e207cd26be3fa89b2df7211a7dbc02ad2fcd7
Brent Kie <brent.a.kie@gmail.com>
2018-04-18 15:22:12 -0600

    Created HTML layout for pwp milestone 1.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<title>Demo Project</title>
+	</head>
+	<body>
+		<h1>PWP Milestone 1</h1>
+			<h2>Purpose, Audience, Goal</h2>
+				<h3>Purpose</h3>
+				<h3>Audience</h3>
+				<h3>Goal</h3>
+			<h2>Persona</h2>
+	</body>
+</html>
