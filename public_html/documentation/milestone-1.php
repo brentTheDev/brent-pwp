ba8c76820f3f2e03210bf55391e406adc9c3bbce
Brent Kie <brent.a.kie@gmail.com>
2018-04-18 21:22:27 -0600

    Updated persona section. Ready for review.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -18,5 +18,24 @@
 					on the website will provide the target audience multiple channels to connect with me. The secondary goal of this
 					website is to showcase my work and contributions to projects as well as creating a professional online presence. </p>
 			<h2>Persona</h2>
+				<ul>
+					<li><strong>Name:</strong> Heidi Dennison</li>
+					<li><strong>Age:</strong> 30</li>
+					<li><strong>Gender:</strong> Female</li>
+					<li><strong>Ethnicity:</strong> Caucasian</li>
+					<li><strong>Occupation:</strong> Project Manager of a tech company focused on improving music software for music producers.</li>
+					<li><strong>Technology:</strong> <em>Phone</em>; Samsung Galaxy S9. <em>Laptop</em>; Lenovo Yoga 920 <em>OS</em>: Windows 10 Pro</li>
+					<li><strong>Attitudes &amp; Behaviors:</strong> Avid social media user, goes to tech mixers/events to seek
+					possible candidates to work with her team, works remotely twice a week, often checks GitHub pulses when connected with a potential candidate.</li>
+					<li><strong>Frustrations &amp; Needs:</strong> Heidi is looking to hire a junior level developer with at least 2 years of experience in IT with
+					1 year of coding experience. She is looking for someone who is musically inclined and has knowledge of digital audio workstation software. She is
+					also looking for someone who has a good personality, works well in group settings and is a self-starter. She does not like to micromanage.
+					She must hire someone within 30 days so that she can begin the next phase of her project.</li>
+					<li><strong>Goals:</strong> Heidi is more concerned with finding the right fit when it comes to the position she must fill.
+					Heidi is willing to set up the position so that the new hire is able to work closely with a senior developer to understand what is happening
+					within the project. Heidi also wants to see that the potential candidate is active through social media channels.</li>
+					<li><strong>User Story:</strong> As a user, I need to decide if the potential candidate has a the right personality,
+					drive, and music knowledge for the position she is looking to fill.</li>
+		</ul>
 	</body>
 </html>
