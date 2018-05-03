921e1645d6df829f30eea0b2b454cf772ac1d858
Brent Kie <brent.a.kie@gmail.com>
2018-05-02 20:49:21 -0600

    Added milestone 2a to project. Added WELCOME section

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>PWP Milestone 2</title>
+	</head>
+	<body>
+		<h1>PWP Milestone 2a</h1>
+			<h2>Content Strategy</h2>
+				<h3>Welcome:</h3>
+					<p>
+						A bold, clean typeface for the name brentTheDev. 2 words that describe the roles that brentTheDev
+						plays as a creator and a developer. This will be overlaid onto a picture with my personal tech utilities
+						(i.e. laptop, idea book) and Indigenous items. This will give the visitor a first impression through
+						visual cues.
+					</p>
+
+	</body>
+</html>
