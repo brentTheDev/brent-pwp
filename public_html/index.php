47f35dd2dd640f079958e42d6ac51871e70449e4
Brent Kie <brent.a.kie@gmail.com>
2018-05-20 11:01:20 -0600

    adjusted layout for portfolio section.

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,9 @@
 		<meta charset="UTF-8">
 		<title> @brentTheDev | Creator/Developer </title>
 
+		<!--Custom CSS-->
+		<link rel="stylesheet" type="text/css" href="styles.css" media="screen" />
+
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
@@ -21,7 +24,7 @@
 	<body>
 		<header>
 			<div class="container">
-				<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white">
+				<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white shadow-sm rounded">
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<span class="navbar-toggler-icon"></span>
 					</button>
@@ -66,23 +69,56 @@
 			</div>
 		</header>
 		<section>
-			<div class="container">
-				<div class="jumbotron">
+			<div class="container mt-5 p-3">
+				<div class="jumbotron mt-5 bg-image=">
 					<h1 class="display-4">Hello, world!</h1>
 					<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 					<hr class="my-4">
 					<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-					<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 				</div>
 			</div>
 		</section>
-		<section class="bg-secondary py-5">
+		<section class="bg-white py-5">
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-3">
-						<img class="img-fluid rounded-circle mb-4" src="bart.jpg" alt="fractal">
+					<div class="col-sm-12 p-3">
+						<h1 class="font-weight-bold">influence</h1>
 					</div>
-					<div class="col-sm-9">
+					<div class="col-sm-4">
+						<img class="img-fluid mb-4" src="bart.jpg" alt="fractal">
+					</div>
+					<div class="col-sm-8">
+						<p class="lead">Allow me to reintroduce myself. I invented swag. Poppin' bottles, puttin' supermodels in the cab.
+							Yo, I'm makin' short term goals, when the weather folds. Just put away the leathers and put ice on
+							the gold. Chilly with enough bail money to free a big Willy. High stakes, I got more at stake than
+							Philly. Let's stick up the world and split it 50/50. Photo shoot fresh, looking like wealth, 'bout to
+							call the papparazzi on myself. Can't leave rap alone, the game needs me. I'm outchere ballin', I know
+							you see my sneaks. Look I'm on my grind cousin, ain't got time for frontin, sensitive thugs ya'll all need hugs.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="bg-white py-5">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 p-3">
+						<h1 class="text-right font-weight-bold">projects</h1>
+					</div>
+					<div class="col-sm-8">
+						<p class="lead">Allow me to reintroduce myself. I invented swag. Poppin' bottles, puttin' supermodels in the cab.
+							Yo, I'm makin' short term goals, when the weather folds. Just put away the leathers and put ice on
+							the gold. Chilly with enough bail money to free a big Willy. High stakes, I got more at stake than
+							Philly. Let's stick up the world and split it 50/50. Photo shoot fresh, looking like wealth, 'bout to
+							call the papparazzi on myself. Can't leave rap alone, the game needs me. I'm outchere ballin', I know
+							you see my sneaks. Look I'm on my grind cousin, ain't got time for frontin, sensitive thugs ya'll all need hugs.</p>
+					</div>
+					<div class="col-sm-4">
+						<img class="img-fluid mb-4" src="bart.jpg" alt="fractal">
+					</div>
+					<div class="col-sm-4">
+						<img class="img-fluid mb-4" src="bart.jpg" alt="fractal">
+					</div>
+					<div class="col-sm-8">
 						<p class="lead">Allow me to reintroduce myself. I invented swag. Poppin' bottles, puttin' supermodels in the cab.
 							Yo, I'm makin' short term goals, when the weather folds. Just put away the leathers and put ice on
 							the gold. Chilly with enough bail money to free a big Willy. High stakes, I got more at stake than
@@ -96,8 +132,8 @@
 		<section>
 			<div class="container">
 				<div class="row">
-					<div class="col-lg-3 col-sm-6">
-						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="bart.jpg" alt="Card image cap">
+					<div class="col-lg-4 col-sm-6">
+						<img class="img-fluid d-block mx-auto mb-4" src="bart.jpg" alt="Card image cap">
 						<div class="card mb-4">
 							<div class="card-body">
 								<h5 class="card-title">Card title</h5>
@@ -105,8 +141,8 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-lg-3 col-sm-6">
-						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="bart.jpg" alt="Card image cap">
+					<div class="col-lg-4 col-sm-6">
+						<img class="img-fluid d-block mx-auto mb-4" src="bart.jpg" alt="Card image cap">
 						<div class="card mb-4">
 							<div class="card-body">
 								<h5 class="card-title">Card title</h5>
@@ -114,17 +150,8 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-lg-3 col-sm-6">
-						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="bart.jpg" alt="Card image cap">
-						<div class="card mb-4">
-							<div class="card-body">
-								<h5 class="card-title">Card title</h5>
-								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-3 col-sm-6">
-						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="bart.jpg" alt="Card image cap">
+					<div class="col-lg-4 col-sm-6">
+						<img class="img-fluid d-block mx-auto mb-4" src="bart.jpg" alt="Card image cap">
 						<div class="card mb-4">
 							<div class="card-body">
 								<h5 class="card-title">Card title</h5>
@@ -135,7 +162,7 @@
 				</div>
 			</div>
 		</section>
-		<footer class="bg-dark text-white text-center">
+		<footer class="bg-white text-center">
 			<div class="container">
 				Please love me alone
 			</div>
