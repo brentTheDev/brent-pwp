89ea19831b8b6ed088feb07d04fdd83b79608bc4
Brent Kie <brent.a.kie@gmail.com>
2018-05-19 19:59:23 -0600

    Made adjustments to navbar.

---
+++ public_html/index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title> @brentTheDev | Creator/Developer </title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+		<!-- Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-white">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item">
+								<a class="nav-link" href="#">&#64;brentTheDev</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">&#47;influence</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#" >&#47;projects</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">&#47;contact</a>
+							</li>
+						</ul>
+					</div>
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="#">Twitter</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">GitHub</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#" >LinkedIn</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<section>
+			<div class="container">
+				<div class="jumbotron">
+					<h1 class="display-4">Hello, world!</h1>
+					<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+					<hr class="my-4">
+					<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+					<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+				</div>
+			</div>
+		</section>
+		<section class="bg-secondary py-5">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3">
+						<img class="img-fluid rounded-circle mb-4" src="bart.jpg" alt="fractal">
+					</div>
+					<div class="col-sm-9">
+						<p class="lead">Allow me to reintroduce myself. I invented swag. Poppin' bottles, puttin' supermodels in the cab.
+							Yo, I'm makin' short term goals, when the weather folds. Just put away the leathers and put ice on
+							the gold. Chilly with enough bail money to free a big Willy. High stakes, I got more at stake than
+							Philly. Let's stick up the world and split it 50/50. Photo shoot fresh, looking like wealth, 'bout to
+							call the papparazzi on myself. Can't leave rap alone, the game needs me. I'm outchere ballin', I know
+							you see my sneaks. Look I'm on my grind cousin, ain't got time for frontin, sensitive thugs ya'll all need hugs.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="bart.jpg" alt="Card image cap">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="bart.jpg" alt="Card image cap">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="bart.jpg" alt="Card image cap">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-sm-6">
+						<img class="img-fluid rounded-circle d-block mx-auto mb-4" src="bart.jpg" alt="Card image cap">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h5 class="card-title">Card title</h5>
+								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer class="bg-dark text-white text-center">
+			<div class="container">
+				Please love me alone
+			</div>
+		</footer>
+	</body>
+</html>
