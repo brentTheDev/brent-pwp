f3cb1e605b5612f5010909a4a2917841c50fbd50
Brent Kie <brent.a.kie@gmail.com>
2018-06-06 02:54:38 -0600

    A lot of changes made. Layout, formatting, addition of images and a directory structure change.

--- public_html/index.php
+++ public_html/index.php
@@ -2,7 +2,9 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title> @brentTheDev | Creator/Developer </title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<title>&#64;brentTheDev &#124; Developer&#47;Creator</title>
 
 		<!--Custom CSS-->
 		<link rel="stylesheet" type="text/css" href="styles.css" media="screen" />
@@ -44,7 +46,7 @@
 					<div class="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul class="navbar-nav mr-auto">
 							<li class="nav-item">
-								<a class="nav-link" href="#">&#64;brentTheDev</a>
+								<a class="nav-link" href="#">brentTheDev</a>
 							</li>
 							<li class="nav-item">
 								<a class="nav-link" href="#influence">&#47;influence</a>
@@ -88,74 +90,97 @@
 		</header>
 		<section>
 			<div class="container mt-5 p-3">
-				<div class="jumbotron mt-5 bg-image=">
-					<h1 class="display-4">Hello, world!</h1>
-					<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-					<hr class="my-4">
-					<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+				<div class="jumbotron mt-5 bg-img">
+					<h1 class="display-4 text-white text-right">brentTheDev</h1>
+					<p class="lead text-white text-right">Developer/Creator</p>
 				</div>
 			</div>
 		</section>
 		<section id="influence" class="bg-white py-5">
 			<div class="container py-3">
-				<div class="row shadow p-3 mb-5 bg-white rounded">
+				<div class="row shadow p-3 bg-white rounded">
 					<div class="col-sm-12 p-3">
 						<h1 class="font-weight-bold">&#47;influence</h1>
 					</div>
 					<div class="col-sm-4">
-						<img class="img-fluid mb-4 bw" src="looks.jpg" alt="laptop">
+						<img class="img-fluid mb-4 bw" src="img/looks.jpg" alt="laptop">
 					</div>
 					<div class="col-sm-8">
-						<p class="lead">Allow me to reintroduce myself. I invented swag. Poppin' bottles, puttin' supermodels in the cab.
-							Yo, I'm makin' short term goals, when the weather folds. Just put away the leathers and put ice on
-							the gold. Chilly with enough bail money to free a big Willy. High stakes, I got more at stake than
-							Philly. Let's stick up the world and split it 50/50. Photo shoot fresh, looking like wealth, 'bout to
-							call the papparazzi on myself. Can't leave rap alone, the game needs me. I'm outchere ballin', I know
-							you see my sneaks. Look I'm on my grind cousin, ain't got time for frontin, sensitive thugs ya'll all need hugs.</p>
+						<p class="lead">The inspiration behind the work is influenced by my love for analyzing data, curating concepts, &amp; strengthening community.
+							As an Indigenous member from the Pueblo of Laguna, cultural values teach us how we are all connected. If we empower one another
+							with knowledge, we are able to share ideas, foster growth, and create change for the greater good of people.
+						</p>
 					</div>
 				</div>
 			</div>
 		</section>
 		<section id="projects" class="bg-white py-5">
 			<div class="container py-3">
-				<div class="row">
+				<div class="row shadow p-1 mb-4 bg-white rounded">
 					<div class="col-sm-12 p-3">
 						<h1 class="text-right font-weight-bold">&#47;projects</h1>
 					</div>
-					<div class="col-sm-8">
+					<div class="col-sm-7 pt-3 pb-3">
 						<h3>onTap</h3>
-						<p class="lead">onTap is a web application built during my time spent at Deep Dive Coding.
-						</p>
-					</div>
-					<div class="col-sm-4">
-						<img class="img-fluid mb-4" src="bart.jpg" alt="fractal">
-					</div>
-					<div class="col-sm-8">
-						<h3>Sum&#46;Days</h3>
-						<p class="lead">One of my favorite projects, Sum.Days was created from talking about Sunday vibes and sharing music with others
-							As BKNights, I teamed up with Jon aka JON to provide a weekly Sunday music podcast show via <a href="https://www.mixlr.com" target="_blank">Mixlr&#46;com.</a> We created our online presence
-							through social media to reach various demographics and gain a following of listeners who tuned into our show during the 4pm-6pm,
-							MST timeslot. This helped craft skills around the use of studio equipment, social media, and music curation. Playback to archived shows are available on Mixcloud. Check out the links
-							to this project.</p>
+						<p class="lead">onTap is a web application built during my time enrolled at Deep Dive Coding. This app was created to match
+							potential patrons with breweries that have beer styles which the patron is looking to enjoy. Created with full stack web development technologies &amp; tools,
+							our application is designed with modern features &amp; security standards throughout the site. As part of a 4-person team, we practiced object-oriented programming, test driven development, &amp;
+							agile methodologies. A working prototype of our application was developed in 7 weeks during Spring of 2018. Thanks to Carlos, James, &amp; Erik for a great experience. Click the link below to see what we
+							created. Thanks!</p>
 						<div class="row justify-content-center">
-							Mixcloud &#124; Twitter &#124; Instagram
+							<p><a href="#" class="text-info">Website Goes Here</a></p>
 						</div>
 					</div>
-					<div class="col-sm-4">
-						<img class="img-fluid mb-4" src="sumdays.png" alt="fractal">
+					<div class="col-sm-5 pt-3 pb-3">
+						<img class="img-fluid shadow p-1 mb-4 bg-white rounded" src="img/on-tap.jpg" alt="beer tap"/>
+					</div>
+					<div class="col-sm-7 pt-3 pb-3">
+						<h3>STEAM NM</h3>
+						<p class="lead">Through a collaboration with the <a href="https://www.nationalservice.gov/programs/americorps/americorpsvista" target="_blank" class="text-info">AmeriCorps VISTA</a> program &amp; <a href="https://steamnm.weebly.com/" target="_blank" class="text-info">STEAM NM</a>, I was placed at Deep Dive Coding as part of an initiative which incorporates art with STEM to create STEAM.
+						Alongside a team of individuals who are making this initiative come to life, we developed relationships between local institutions, government entities, entrepreneurs, &amp; artists to provide a pathway that nourishes the growth of STEAM. My time spent at Deep Dive Coding
+						was utilized by developing &amp; curating content for their social media channels. I was also able to provide information in regards to programs offered by Deep Dive Coding such as financial assistance,
+						course content, &amp; general inquires. Click the links below to see what's happening at Deep Dive Coding!</p>
+						<div class="row justify-content-center">
+							<ul class="list-inline">
+								<li><a href="https://www.deepdivecoding.com" target="_blank" class="text-info">DeepDiveCoding.com</a></li>
+								<li><a href="https://www.twitter.com/deepdivecoding" target="_blank" class="text-info">&#64;DeepDiveCoding <i class="fab fa-twitter"></i></a></li>
+								<li><a href="https://www.instagram.com/deepdivecoding" target="_blank" class="text-info">&#64;DeepDiveCoding <i class="fab fa-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-5 pt-3 pb-3">
+						<img class="img-fluid shadow p-1 mb-4 bg-white rounded" src="img/steamnm.jpg" alt="STEAM NM"/>
+					</div>
+					<div class="col-sm-7 pt-3 pb-3">
+						<h3>Sum&#46;Days</h3>
+						<p class="lead">One of my favorite projects, Sum.Days was created from Sunday vibes &amp; sharing music with others.
+							As BKNights, I teamed up with Jon aka JON to provide a weekly Sunday music podcast show via live stream app, <a href="https://www.mixlr.com" target="_blank" class="text-info">Mixlr</a>. We created our online presence
+							through social media to reach various demographics &amp; gain a following of listeners who tuned into our show during the 4pm-6pm,
+							MST timeslot. This helped craft skills around the use of studio equipment, social media, &amp; music curation. Playback to archived shows are available on Mixcloud <i class="fab fa-mixcloud"></i>. Click the links
+							below to hear &amp; view more of this project.</p>
+						<div class="row justify-content-center">
+							<ul class="list-inline">
+								<li><a href="https://www.mixcloud.com/sumdays" target="_blank" class="text-info"> Sum.Days <i class="fab fa-mixcloud"></i></a></li>
+								<li><a href="https://www.twitter.com/sum_days" target="_blank" class="text-info">&#64;Sum_Days <i class="fab fa-twitter"></i></a></li>
+								<li><a href="https://www.instagram.com/sum.days" target="_blank" class="text-info">&#64;Sum.Days <i class="fab fa-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-5 pt-3 pb-3">
+						<img class="img-fluid img-fluid shadow p-1 mb-4 bg-white rounded" src="img/sumdays.png" alt="sumdays"/>
 					</div>
 				</div>
 			</div>
 		</section>
 		<section id="contact" class="bg-white py-5">
 			<div class="container py-3">
-				<div class="row">
+				<div class="row shadow p-3 mb-5 bg-white rounded">
 					<div class="col-sm-12 p-3">
 						<h1 class="font-weight-bold text-center">&#47;contact</h1>
 					</div>
 					<div class="col-sm-6">
-						<p class="lead">Feel free to send me a message about business inquiries or project collaboration. Don't forget to follow me on Twitter &amp; LinkedIn
-						I look forward to hearing from you! Have a great day! Da'waa'eh(thanks)!</p>
+						<p class="lead">I appreciate you stopping by my website. Feel free to send me a message about business inquiries or project collaboration. Don't forget to follow me on Twitter &amp; LinkedIn!
+						I look forward to hearing from you! Da'waa'eh(thanks)!</p>
 					</div>
 					<div class="col-sm-6">
 						<!-- BEGIN CONTACT FORM -->
@@ -209,9 +234,9 @@
 				</div>
 			</div>
 		</section>
-		<footer class="bg-white text-center">
+		<footer class="bg-white text-center pb-3">
 			<div class="container">
-				Please love me alone
+				&copy; 2018 &#124; brentTheDev
 			</div>
 		</footer>
 	</body>
